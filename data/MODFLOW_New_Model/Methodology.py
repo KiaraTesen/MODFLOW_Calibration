c0c46f12ff0e0c7b63c76507a39e5be2dd1f3a6a
KiaraTesen <katesen@uc.cl>
2023-03-27 18:30:17 -0300

    Update_27_3

--- data/MODFLOW_New_Model/Methodology.py
+++ data/MODFLOW_New_Model/Methodology.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 17 2023
-@author: Kiara Tesen
-"""
 
 #---    Packages
 import geopandas as gpd
 import pandas as pd
 import numpy as np
-import flopy.modflow as fpm
+import matplotlib.pyplot as plt
 import os
+from functools import reduce
+from scipy import signal
+import flopy.modflow as fpm
 import shutil
+
 import win32com.client as win32
+
+from sklearn.metrics import mean_squared_error
+import math
 
 #---    Paths
 path_WEAP = r'C:\Users\Francisco Suárez P\Documents\WEAP Areas\SyntheticProblem_WEAPMODFLOW'
@@ -19,46 +22,79 @@ path_model = r'..\MODFLOW_Model'
 path_nwt_exe = r'..\MODFLOW-NWT_1.2.0\bin\MODFLOW-NWT_64.exe'
 path_GIS = r'..\GIS'
 path_output = r'C:\Users\Francisco Suárez P\Desktop\GitHub - KT\MODFLOW_Calibration\data\output' # Necesita ruta completa por WEAP Export
+path_obs_data = r'..\ObservedData'
 
 iteration = 1
-
 dir_iteration = os.path.join(path_output, "iter_" + str(iteration))
 if not os.path.isdir(dir_iteration):
     os.mkdir(dir_iteration)
-"""
+
 #-------------------------------------------------------------
 #---    Modification of Hydraulic Properties - MODFLOW    ----
 #-------------------------------------------------------------
+variables = ['kx', 'kz', 'sy']
 
-#K_values = [0.328, 0.512, 0.656, 0.82, 1.28, 1.64, 2.56, 3.28, 4.48, 5.12, 6.56, 8.2, 12.8, 16.4, 25.6, 32.8, 44.8, 51.2, 65.6]
-
+#---    Visualization of the matriz
+def get_image_matriz(matriz, variable, path_out):
+    fig=plt.figure(figsize = (16,8))
+    ax = plt.axes()
+    im = ax.imshow(matriz)
+    plt.title(variable)
+    plt.xlabel('Column')
+    plt.ylabel('Row')
+    cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])
+    plt.colorbar(im, cax=cax)
+    plt.savefig(path_out)
+    
 #---    Generate new shape of HP
-def get_HP(Shape_HP, variable):
+def get_HP(Shape_HP, variable, active_matriz):
     rows = Shape_HP["ROW"].max()
     columns = Shape_HP["COLUMN"].max()
 
+    #---    Particle Swarm Optimization (PSO)
+    kernel_shape = (5,5)
+    n_kernels = 1
+    n_hp = 3
+    n_var = reduce(lambda x,y: x*y, kernel_shape)*n_kernels*n_hp
+
+    kernel = np.random.uniform(0.9, 1.1, reduce(lambda x,y: x*y, kernel_shape)).reshape(kernel_shape)
+
     matriz = np.zeros((rows,columns))
     for i in range(0,len(Shape_HP['ROW'])):
-        matriz[Shape_HP['ROW'][i]-1][Shape_HP['COLUMN'][i]-1] = Shape_HP[variable][i] * 5 # AQUÍ HACER LA OPERACION
-    return matriz
+        matriz[Shape_HP['ROW'][i]-1][Shape_HP['COLUMN'][i]-1] = Shape_HP[variable][i] # AQUÍ HACER LA OPERACION
 
-original_shape_HP = gpd.read_file(path_GIS + '/Elements_FA.shp') # El shape que se use depende del mejor resultado (?)
-variables = ['kx', 'kz', 'ss', 'sy']
-new_shape_HP = original_shape_HP
+        #---    Convolution
+        new_matriz = signal.convolve2d(matriz, kernel, boundary = 'symm', mode = 'same')
+        new_matriz = new_matriz * active_matriz
 
-for i in variables:
-    locals()["matriz_" + str(i)] = get_HP(original_shape_HP, str(i)) # El shape que se use depende del mejor resultado (?)
-    vector = locals()["matriz_" + str(i)].flatten()
-    new_shape_HP[i] = vector
-#print(matriz_kx)
-#print(new_shape_HP)
+    return new_matriz
 
-#---    Save new shape of HP
-dir_iteration = os.path.join(path_output, "iter_" + str(iteration))
-if not os.path.isdir(dir_iteration):
-    os.mkdir(dir_iteration)
-new_shape_HP.to_file(os.path.join(dir_iteration, 'Elements_FA_iter_' + str(iteration) + '.shp'))
+#---    Initial matriz
+initial_shape_HP = gpd.read_file(path_GIS + '/Elements_init.shp') # El shape que se use depende del mejor resultado (?)
+active_matriz = initial_shape_HP['Active'].to_numpy().reshape((84,185))
 
+for k in variables:
+    locals()["init_matriz_" + str(k)] = initial_shape_HP[k].to_numpy().reshape((84,185))
+    get_image_matriz(locals()["init_matriz_" + str(k)], str(k), os.path.join(path_GIS, str(k) +'.png'))
+    plt.clf()
+
+#---    Modified matriz
+new_shape_HP = initial_shape_HP
+for m in variables:
+    locals()["matriz_" + str(m)] = get_HP(initial_shape_HP, str(m), active_matriz)
+    get_image_matriz(locals()["matriz_" + str(m)], str(m), os.path.join(dir_iteration, str(m) +'.png'))
+    plt.clf()
+
+    locals()["vector_" + str(m)] = locals()["matriz_" + str(m)].flatten()
+    new_shape_HP[m] = locals()["vector_" + str(m)]
+
+matriz_ss = matriz_sy/100
+vector_ss = matriz_ss.flatten()
+new_shape_HP['ss'] = vector_ss
+
+new_shape_HP.to_file(os.path.join(dir_iteration, 'Elements_iter_' + str(iteration) + '.shp'))
+
+"""
 #-----------------------------------------
 #---    New native files - MODFLOW    ----
 #-----------------------------------------
@@ -91,7 +127,7 @@ for h in get_new_files:
         pass
     else:
         shutil.copy(os.path.join(os.getcwd(), h), os.path.join(path_MODFLOW_WEAP, h))
-"""
+
 #-------------------------------------
 #---    Run WEAP-MODFLOW model    ----
 #-------------------------------------
@@ -107,10 +143,79 @@ favorites = pd.read_excel("../Favorites_WEAP.xlsx")
 for i,j in zip(favorites["BranchVariable"],favorites["WEAP Export"]):
     WEAP.LoadFavorite(i)
     WEAP.ExportResults(os.path.join(dir_iteration, f"iter_{str(iteration)}_{j}.csv"), True, True, True, False, False)
-
+"""
 #---------------------------------
 #---    Objective Function    ----
 #---------------------------------
 
+def get_data(path,skr):
+    df_data = pd.read_csv(path, skiprows = skr)
+    df_data = df_data.set_index('Branch')
+    df_data = df_data.set_index(pd.to_datetime(df_data.index))
+    df_data = df_data.iloc[36:,:]
+    return df_data
+
+#---    Well analysis
+obs_well = get_data(os.path.join(path_obs_data, 'Wells_observed.csv'), 3)
+ow = obs_well.columns
+
+sim_well = get_data(os.path.join(dir_iteration, f"iter_{str(iteration)}_Wells_simulation.csv"), 3)
+
+srmse_well = 0
+for i in ow:
+    mse_well = mean_squared_error(obs_well[i], sim_well[i])
+    rmse_well = math.sqrt(mse_well)
+    #print(f"RMSE - well {i}: {rmse_well}")
+    srmse_well += rmse_well
+#print(f"SRMSE - well: {srmse_well}")
+
+#---    Streamflow analysis
+df_q = pd.read_csv(os.path.join(dir_iteration, f"iter_{str(iteration)}_Streamflow_gauges.csv"), skiprows = 3)
+mse_q = mean_squared_error(df_q['Observed'], df_q['Modeled'])
+rmse_q = math.sqrt(mse_q)
+#print(f"SRMSE - q: {rmse_q}")
+
 #---    Subject to
+kx_min = 0.280
+kx_max = 67.056
+sy_min = 0.0074
+sy_max = 0.1282
+
+def get_eliminate_zeros(lista):
+    position = 0
+    while position < len(lista):
+        if lista[position] == 0:
+            lista.pop(position)
+        else:
+            position += 1
+    return lista 
+
+def get_evaluate_st_bounds(min_v, max_v, vector_modif):
+    if min_v > min(vector_modif):
+        P_min = min_v - min(vector_modif)
+    else:
+        P_min = 0
+
+    if max_v < max(vector_modif):
+        P_max = max(vector_modif) - max_v
+    else:
+        P_max = 0
+    return P_min + P_max
+
+for n in variables:
+    if n == 'kz':
+        pass
+    else:
+        locals()["vector_modif_" + str(n)] = get_eliminate_zeros(locals()["vector_" + str(n)].tolist())
+        locals()["P_" + str(n)] = get_evaluate_st_bounds((locals()[str(n) + "_min"]), (locals()[str(n) + "_max"]), locals()["vector_modif_" + str(n)])
+
+#---    Total Objective Function
+gw = 0.8
+gq = 1 - gw
+gk = 0.8
+gs = 1 - gk
+
+of = gw*srmse_well + gq*rmse_q + gk*P_kx + gs*P_sy
+print(f"Objective function: {of}")
+
 
