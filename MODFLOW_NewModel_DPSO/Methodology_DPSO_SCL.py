0491063472a7784097c8746a92f251db96f7b3b3
KiaraTesen <katesen@uc.cl>
2023-06-21 13:06:22 -0400

    Actualiza Methodology DPSO_SCL

--- MODFLOW_NewModel_DPSO/Methodology_DPSO_SCL.py
+++ MODFLOW_NewModel_DPSO/Methodology_DPSO_SCL.py
@@ -34,7 +34,7 @@ with open('log_iteration.txt') as f:
 #---    Initial matriz
 HP = ['kx', 'sy'] 
 initial_shape_HP = gpd.read_file(path_GIS + '/Elements_initial_unique_value.shp')
-print(initial_shape_HP)
+#print(initial_shape_HP)
 active_matriz = initial_shape_HP['Active'].to_numpy().reshape((84,185))             # Matrix of zeros and ones that allows maintaining active area
 
 n = 1                                                           # Population size
