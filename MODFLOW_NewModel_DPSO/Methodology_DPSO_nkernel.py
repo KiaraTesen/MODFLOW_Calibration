0ca1d629bdef76b91fefe008e8f284e183fe5fb3
KiaraTesen <katesen@uc.cl>
2023-07-04 13:05:53 -0400

    Prueba nkernels_60_5_3

--- MODFLOW_NewModel_DPSO/Methodology_DPSO_nkernel.py
+++ MODFLOW_NewModel_DPSO/Methodology_DPSO_nkernel.py
@@ -32,7 +32,7 @@ active_matriz = initial_shape_HP['Active'].to_numpy().reshape((84,185))         
 
 n = 1                                                           # Population size
 
-k_shape_1 = (50,50)
+k_shape_1 = (60,60)
 k_shape_2 = (5,5)
 k_shape_3 = (3,3)
 
