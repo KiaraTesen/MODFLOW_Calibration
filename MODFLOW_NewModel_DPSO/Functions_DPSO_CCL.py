f01e198cb6deb5ee4c981957f064ef723ad45c7e
Kiara Tesen <katesen@uc.cl>
2023-07-11 15:42:37 -0400

    Corrige

--- MODFLOW_NewModel_DPSO/Functions_DPSO_CCL.py
+++ MODFLOW_NewModel_DPSO/Functions_DPSO_CCL.py
@@ -220,4 +220,4 @@ def Run_WEAP_MODFLOW(path_output, iteration, initial_shape_HP, HP, sample_scaled
     #of = g1*srmse_well + g2*rmse_q
     of = g_srmse_well + g2*rmse_q + g3*(P_kx + P_sy)
     return of
-"""
+    """
