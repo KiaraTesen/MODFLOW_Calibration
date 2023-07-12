afc414b332b7a904717275508d9fa48e86f2ad97
Kiara Tesen <katesen@uc.cl>
2023-07-11 23:34:09 -0400

    DPSO_SCL_CCL_run

--- MODFLOW_NewModel_DPSO/Functions_DPSO_SCL_CCL.py
+++ MODFLOW_NewModel_DPSO/Functions_DPSO_SCL_CCL.py
@@ -152,5 +152,104 @@ def Run_WEAP_MODFLOW(path_output, iteration, initial_shape_HP, HP, active_cells,
     new_shape_HP['kz'] = matriz_kz.flatten()
     new_shape_HP['ss'] = matriz_ss.flatten()
     new_shape_HP.to_file(os.path.join(dir_iteration, 'Elements_iter_' + str(iteration) + '.shp'))
-    print(new_shape_HP)
-    print(initial_shape_HP)
+
+    #---    Generate new native files
+    model = fpm.Modflow.load(path_init_model + '/SyntheticAquifer_NY.nam', version = 'mfnwt', exe_name = path_nwt_exe)
+    model.write_input()
+    model.remove_package("UPW")
+    upw = fpm.ModflowUpw(model = model, laytyp=1, layavg=0, chani=-1.0, layvka=0, laywet=0, hdry=-888, iphdry=1, hk=matriz_kx, hani=1.0, vka=matriz_kz, ss=matriz_ss, sy=matriz_sy, extension='upw')
+    upw.write_file()
+    model.run_model()
+    
+    #---    Move native files to WEAP
+    get_old_files = os.listdir(path_model)
+    get_new_files = os.listdir(os.getcwd())
+
+    #---    Delete old files
+    for g in get_old_files:
+        try:
+            os.remove(os.path.join(path_model, g))
+        except:
+            print('No hay archivos')
+
+    #---    Move new files
+    for h in get_new_files:
+        if h.endswith('.py') or h == '__pycache__' or h == 'sp' or h.endswith('.txt') or h == 'output' or h.endswith('.ps1') or h.endswith('.h5') or h == 'PRUEBAAAAAAS':
+            pass 
+        else:
+            shutil.move(os.path.join(os.getcwd(), h), os.path.join(path_model, h))
+    
+    #-------------------------------------
+    #---    Run WEAP-MODFLOW model    ----
+    #-------------------------------------
+    WEAP = win32.Dispatch("WEAP.WEAPApplication")
+    WEAP.ActiveArea = "SyntheticProblem_WEAPMODFLOW"
+    WEAP.Calculate()
+
+    #---    Export results
+    favorites = pd.read_excel(r"C:\Users\vagrant\Documents\MODFLOW_Calibration\data\Favorites_WEAP.xlsx")
+    for i,j in zip(favorites["BranchVariable"],favorites["WEAP Export"]):
+        WEAP.LoadFavorite(i)
+        WEAP.ExportResults(os.path.join(dir_iteration, f"iter_{str(iteration)}_{j}.csv"), True, True, True, False, False)
+
+    #---------------------------------
+    #---    Objective Function    ----
+    #---------------------------------
+    #---    Well analysis
+    obs_well = get_data(os.path.join(path_obs_data, 'Wells_observed.csv'), 3)
+    ow = obs_well.columns
+    #print(ow)
+
+    sim_well = get_data(os.path.join(dir_iteration, f"iter_{str(iteration)}_Wells_simulation.csv"), 3)
+
+    g_srmse_well = 0
+    srmse_well = 0
+    for i in ow:
+        if i == "OW51" or i == "OW87" or i == "OW97" or i == "OW100" or i == "OW157" or i == "OW167" or i == "OW181" or i == "OW188" or i == "OW233" or i == "OW234" or i == "OW235":
+            g = 0.6 # 0.8
+            #print(i, g)
+        else:
+            g = 0.6
+            #print(i, g) 
+
+        mse_well = mean_squared_error(obs_well[i], sim_well[i])
+        rmse_well = math.sqrt(mse_well)
+        g_rmse_well = g * rmse_well
+
+        srmse_well += rmse_well
+        g_srmse_well += g_rmse_well
+    #print(srmse_well)
+
+    #---    Streamflow analysis
+    df_q = pd.read_csv(os.path.join(dir_iteration, f"iter_{str(iteration)}_Streamflow_gauges.csv"), skiprows = 3)
+    df_q = df_q.set_index('Statistic')
+    df_q = df_q.set_index(pd.to_datetime(df_q.index))
+    df_q = df_q.iloc[36:,:]
+
+    df_q_obs = get_data(os.path.join(path_obs_data, 'StreamflowGauges_KPR_vf.csv'), 2)
+    
+    mse_q = mean_squared_error(df_q_obs['Observed'], df_q['Modeled'])
+    rmse_q = math.sqrt(mse_q)
+    #print(rmse_q)
+
+    #---    Subject to
+    kx_min = 0.280
+    kx_max = 67.056
+    sy_min = 0.01
+    sy_max = 0.1282
+
+    for i in HP:
+        globals()["vector_modif_" + str(i)] = get_eliminate_zeros(globals()["vector_" + str(i)].tolist())
+        globals()["P_" + str(i)] = get_evaluate_st_bounds((locals()[str(i) + "_min"]), (locals()[str(i) + "_max"]), globals()["vector_modif_" + str(i)])
+
+    #---    Total Objective Function
+    #---    There are 31 observation wells and 1 streamflow gauge (32 monitoring elements. If each of them has the same weighting factor: 1/32 = 0.03125 (3.125%))
+    #g1 = 0.6
+    g2 = 0.4
+    g3 = 0.6
+
+    #of = g1*srmse_well + g2*rmse_q + g3*(P_kx + P_sy)
+    #of = g1*srmse_well + g2*rmse_q
+    of = g_srmse_well + g2*rmse_q + g3*(P_kx + P_sy)
+    return of
+
