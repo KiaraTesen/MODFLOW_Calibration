98e6721f7e2480c4a013d2079dcd8164e885e816
KiaraTesen <katesen@uc.cl>
2023-07-18 13:02:26 -0400

    Prueba4

--- MODFLOW_NewModel_DPSO/Methodology_DPSO_CCL.py
+++ MODFLOW_NewModel_DPSO/Methodology_DPSO_CCL.py
@@ -47,9 +47,9 @@ n_var = 2 * n_var    # Number of variables
 print (n_var)
 
 #---    Bounds
-lb_1_kx, lb_1_sy = 0.0004, 0.05       #0.001, 0.075   
+lb_1_kx, lb_1_sy = 0.0004, 0.075       #0.001, 0.075   
 lb_2_kx, lb_2_sy = 0.004, 0.1   
-ub_1_kx, ub_1_sy = 0.25, 0.15         #0.1, 0.1
+ub_1_kx, ub_1_sy = 0.2, 0.1         #0.1, 0.1
 ub_2_kx, ub_2_sy = 0.3, 0.22
 
 l_bounds = np.concatenate((np.around(np.repeat(lb_1_kx, n_var_1),4), np.around(np.repeat(lb_1_sy, n_var_1),4), 
