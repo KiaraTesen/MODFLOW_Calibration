656dde40a879ad537ea18abcb08895b30476369b
KiaraTesen <katesen@uc.cl>
2023-07-21 12:53:38 -0400

    Prueba Cargo DDE

--- MODFLOW_NewModel_DDE/Cargo_run_DDE.ps1
+++ MODFLOW_NewModel_DDE/Cargo_run_DDE.ps1
@@ -3,9 +3,10 @@ param()
 
 $vm = [int]((hostname) -replace '\D+(\d+)','$1')
 #$vms = [int]($p2)       # Last machine
+$ip = 10+$vm
 
 Write-Host $vm
-cargo run -- --ip 10.0.0.(10+$vm) --port 8888 --service DE --subnet 10.0.0
+cargo run -- --ip "10.0.0.$ip" --port 8888 --service DE --subnet 10.0.0
    
 #for(; $i -le $vms; $i++){
  #  
