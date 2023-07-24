a370efb693ff383ccc6b5e04b9ad00f2ca995886
KiaraTesen <katesen@uc.cl>
2023-07-24 15:47:18 -0400

    P11-DPSO

--- MODFLOW_NewModel_DPSO/Methodology_DPSO_SCL_CCL.py
+++ MODFLOW_NewModel_DPSO/Methodology_DPSO_SCL_CCL.py
@@ -55,9 +55,9 @@ lb_kx, ub_kx = 0.015, 3.8                 # P15 - P18: 0.0005, 3.8
 lb_sy, ub_sy = 0.278, 3.57           # P15 - P18: 1.25, 3.45
 
 lb_1_kx, ub_1_kx = 0.001, 0.1
-lb_1_sy, ub_1_sy = 0.525, 0.6
+lb_1_sy, ub_1_sy = 0.35, 0.45
 lb_2_kx, ub_2_kx = 0.002, 0.3
-lb_2_sy, ub_2_sy = 0.175, 0.20
+lb_2_sy, ub_2_sy = 0.1, 0.15
 
 l_bounds = np.concatenate((np.around(np.repeat(lb_kx, active_cells),4), np.around(np.repeat(lb_sy, active_cells),4), 
                            np.around(np.repeat(lb_1_kx, n_var_1),4), np.around(np.repeat(lb_1_sy, n_var_2),4), 
