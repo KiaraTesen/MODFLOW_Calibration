1870783299e0471fac3c8f9bc851af66b5d54ec7
Kiara Tesen <katesen@uc.cl>
2023-10-26 12:44:55 -0500

    Actualiza metricas

--- Results/3_Obs_vs_Sim_values_HP.py
+++ Results/3_Obs_vs_Sim_values_HP.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 import math
 import hydroeval as he
 import warnings
@@ -63,27 +64,26 @@ DF_q = pd.DataFrame()
 DF_q['Modeled - ADPSO-CL'] = np.array(df_q['Modeled'])
 DF_q['Modeled - ADDE-CL'] = np.array(df_q_2['Modeled'])
 DF_q['Observed'] = np.array(df_q_obs['Observed'])
-#DF_q['Initial value'] = np.array(df_q_init['Modeled'])
 DF_q = DF_q.set_index(pd.to_datetime(df_q.index))
 
 #---    Metrics
-print('DPSO')
-rmse_q = he.evaluator(he.rmse, DF_q['Modeled - ADPSO-CL'], DF_q['Observed'])
-nse_q = he.evaluator(he.nse, DF_q['Modeled - ADPSO-CL'], DF_q['Observed'])
-kge_q, r, alpha, beta = he.evaluator(he.kge, DF_q['Modeled - ADPSO-CL'], DF_q['Observed']) # kge, r, alpha, beta
-pbias_q = he.evaluator(he.pbias, DF_q['Modeled - ADPSO-CL'], DF_q['Observed'])
-mae_q = mean_absolute_error(DF_q['Observed'], DF_q['Modeled - ADPSO-CL'])
-r2_q = r2_score(DF_q['Observed'], DF_q['Modeled - ADPSO-CL'])
-print(round(rmse_q[0],3), round(nse_q[0],3), round(kge_q[0],3), round(r2_q,3))
+rmse_q = mean_squared_error(np.array(df_q_obs['Observed']), np.array(df_q['Modeled']), squared = False)
+nse_q = he.evaluator(he.nse,np.array(df_q['Modeled']), np.array(df_q_obs['Observed']))
+kge_q, r, alpha, beta = he.evaluator(he.kge, np.array(df_q['Modeled']), np.array(df_q_obs['Observed'])) # kge, r, alpha, beta
+pbias_q = he.evaluator(he.pbias, np.array(df_q['Modeled']), np.array(df_q_obs['Observed']))
+mae_q = mean_absolute_error(np.array(df_q_obs['Observed']), np.array(df_q['Modeled']))
+corr_matrix = np.corrcoef(np.array(df_q_obs['Observed']), np.array(df_q['Modeled']))[0,1]
+r_sq = corr_matrix**2
+print('DPSO-Q', round(rmse_q,3), round(mae_q,3), round(kge_q[0],3))
 
-print('DDE')
-rmse_q_2 = he.evaluator(he.rmse, DF_q['Modeled - ADDE-CL'], DF_q['Observed'])
-nse_q_2 = he.evaluator(he.nse, DF_q['Modeled - ADDE-CL'], DF_q['Observed'])
-kge_q_2, r_2, alpha_2, beta_2 = he.evaluator(he.kge, DF_q['Modeled - ADDE-CL'], DF_q['Observed']) # kge, r, alpha, beta
-pbias_q_2 = he.evaluator(he.pbias, DF_q['Modeled - ADDE-CL'], DF_q['Observed'])
-mae_q_2 = mean_absolute_error(DF_q['Observed'], DF_q['Modeled - ADDE-CL'])
-r2_q_2 = r2_score(DF_q['Observed'], DF_q['Modeled - ADDE-CL'])
-print(round(rmse_q_2[0],3), round(nse_q_2[0],3), round(kge_q_2[0],3), round(r2_q_2,3))
+rmse_q_2 = he.evaluator(he.rmse, np.array(df_q_2['Modeled']), np.array(df_q_obs['Observed']))
+nse_q_2 = he.evaluator(he.nse, np.array(df_q_2['Modeled']), np.array(df_q_obs['Observed']))
+kge_q_2, r_2, alpha_2, beta_2 = he.evaluator(he.kge, np.array(df_q_2['Modeled']), np.array(df_q_obs['Observed'])) # kge, r, alpha, beta
+pbias_q_2 = he.evaluator(he.pbias, np.array(df_q_2['Modeled']), np.array(df_q_obs['Observed']))
+mae_q_2 = mean_absolute_error(np.array(df_q_obs['Observed']), np.array(df_q_2['Modeled']))
+corr_matrix_2 = np.corrcoef(np.array(df_q_obs['Observed']), np.array(df_q_2['Modeled']))[0,1]
+r_sq_2 = corr_matrix_2**2
+print('DDE-Q', round(rmse_q_2[0],3), round(mae_q_2,3), round(kge_q_2[0],3),)
 
 #---    Graph
 fig = plt.subplots(figsize=(14, 7))
@@ -128,27 +128,27 @@ sim_well_2 = sim_well_2.set_index(pd.to_datetime(df_q_2.index))
 #print(sim_well)
 
 for p in ow[1:]:
-    print(p)
-
-    print('DPSO')
     #---    Metrics
     rmse_w = he.evaluator(he.rmse, sim_well[p], obs_well[p])
     nse_w = he.evaluator(he.nse, sim_well[p], obs_well[p])
     kge_w, r, alpha, beta = he.evaluator(he.kge, sim_well[p], obs_well[p]) # kge, r, alpha, beta
     pbias_w = he.evaluator(he.pbias, sim_well[p], obs_well[p])
     mae_w = mean_absolute_error(obs_well[p], sim_well[p])
-    r2_w = r2_score(obs_well[p], sim_well[p])
-    print(round(rmse_w[0],3), round(nse_w[0],3), round(kge_w[0],3), round(r2_w,3))
+    corr_matrix_w = np.corrcoef(np.array(obs_well[p]), np.array(sim_well[p]))
+    corr_w = corr_matrix_w[0,1]
+    r_sq_w = corr_w**2
+    print('DPSO-'+p, round(rmse_w[0],3), round(mae_w,3), round(kge_w[0],3))
 
-    print('DDE')
     #---    Metrics
     rmse_w_2 = he.evaluator(he.rmse, sim_well_2[p], obs_well[p])
     nse_w_2 = he.evaluator(he.nse, sim_well_2[p], obs_well[p])
     kge_w_2, r_2, alpha_2, beta_2 = he.evaluator(he.kge, sim_well_2[p], obs_well[p]) # kge, r, alpha, beta
     pbias_w_2 = he.evaluator(he.pbias, sim_well_2[p], obs_well[p])
     mae_w_2 = mean_absolute_error(obs_well[p], sim_well_2[p])
-    r2_w_2 = r2_score(obs_well[p], sim_well_2[p])
-    print(round(rmse_w_2[0],3), round(nse_w_2[0],3), round(kge_w_2[0],3), round(r2_w_2,3))
+    corr_matrix_w_2 = np.corrcoef(np.array(obs_well[p]), np.array(sim_well_2[p]))
+    corr_w_2 = corr_matrix_w_2[0,1]
+    r_sq_w_2 = corr_w_2**2
+    print('DDE-'+p, round(rmse_w_2[0],3), round(mae_w_2,3), round(kge_w_2[0],3))
 
     #---    Graph
     fig = plt.subplots(figsize=(14, 7))
@@ -171,137 +171,3 @@ for p in ow[1:]:
 
     plt.savefig(os.path.join(r'D:\1_PaperI\Graphs', 'Obs_vs_Sim_Well_' + str(p) + '.png'))
     plt.clf()
-
-"""
-
-
-#---    Graph
-fig = plt.subplots(figsize=(10, 6))
-plt.plot(DF_q['Observed'], label = 'Obs', color = "black", linewidth = 0.25)
-plt.plot(DF_q['Modeled'], label = 'DPSO', color = "red", linewidth = 0.25)
-plt.plot(DF_q['Initial value'], label = 'Init', color = "blue", linewidth = 0.25)
-
-ymin = min(DF_q['Modeled'].to_numpy().min(), DF_q['Observed'].to_numpy().min(), DF_q['Initial value'].to_numpy().min())
-ymax = max(DF_q['Modeled'].to_numpy().max(), DF_q['Observed'].to_numpy().max(), DF_q['Initial value'].to_numpy().max())
-dif_v = ymax - ymin
-
-plt.ylim(0, ymax + dif_v)
-plt.ylabel('Streamflow ($m^{3}/s$)', fontsize = 12)
-plt.xlabel('Years', fontsize = 12)
-plt.legend(bbox_to_anchor=(1.0, 1.0), loc='upper left')
-plt.title('Streamflow gauge', fontsize = 16, fontweight='bold')
-
-fs_text = 8
-plt.text(13200, ymax + dif_v - dif_v/8, 'NSE:    ' + str(round(nse_q[0],3)), fontsize = fs_text)
-plt.text(13200, ymax + dif_v - 2*(dif_v/8), 'KGE:    ' + str(round(kge_q[0],3)), fontsize = fs_text)
-plt.text(13200, ymax + dif_v - 3*(dif_v/8), 'PBIAS: ' + str(round(pbias_q[0],3)), fontsize = fs_text)
-plt.text(13200, ymax + dif_v - 4*(dif_v/8), 'RMSE: ' + str(round(rmse_q[0],3)) + ' $m^{3}/s$', fontsize = fs_text)
-plt.text(13200, ymax + dif_v - 5*(dif_v/8), 'MAE:   ' + str(round(mae_q,3)) + ' $m^{3}/s$', fontsize = fs_text)
-plt.text(13200, ymax + dif_v - 6*(dif_v/8), '$R^{2}$:      ' + str(round(r2_q,3)), fontsize = fs_text)
-
-plt.savefig(os.path.join(path_results, best_experiment, 'Graficas', 'Q_' + methodology + '_' + best_experiment + '.png'))
-plt.clf()
-
-#---    Observation wells
-obs_well = pd.read_csv(r'..\data\ObservedData\Wells_observed.csv', skiprows = 3)
-obs_well = obs_well.iloc[36:,:]
-obs_well = obs_well.set_index(pd.to_datetime(df_q.index))
-#print(obs_well)
-
-ow = obs_well.columns
-
-sim_well = pd.read_csv(os.path.join(path_results, best_experiment, best_result, 'iter_' + str(best_iteration), best_w), skiprows = 3)
-sim_well = sim_well.iloc[36:,:]
-sim_well = sim_well.set_index(pd.to_datetime(df_q.index))
-#print(sim_well)
-
-init_well = pd.read_csv(os.path.join(r'..\results_base', elements_init, 'Wells_simulation.csv'), skiprows = 3)
-init_well = init_well.iloc[36:,:]
-init_well = init_well.set_index(pd.to_datetime(df_q.index))
-#print(init_well)
-
-for p in ow[1:]:
-    #---    Metrics
-    rmse_w = he.evaluator(he.rmse, sim_well[p], obs_well[p])
-    nse_w = he.evaluator(he.nse, sim_well[p], obs_well[p])
-    kge_w, r, alpha, beta = he.evaluator(he.kge, sim_well[p], obs_well[p]) # kge, r, alpha, beta
-    pbias_w = he.evaluator(he.pbias, sim_well[p], obs_well[p])
-    mae_w = mean_absolute_error(obs_well[p], sim_well[p])
-    r2_w = r2_score(obs_well[p], sim_well[p])
-
-    #---    Graph
-    fig = plt.subplots(figsize=(10, 6))
-    plt.plot(obs_well[p], label = 'Obs', color = "black", linewidth = 0.25)
-    plt.plot(sim_well[p], label = 'DPSO', color = "red", linewidth = 0.25)
-    plt.plot(init_well[p], label = 'Init', color = "blue", linewidth = 0.25)
-
-    ymin = min(obs_well[p].to_numpy().min(), sim_well[p].to_numpy().min(), init_well[p].to_numpy().min())
-    ymax = max(obs_well[p].to_numpy().max(), sim_well[p].to_numpy().max(), init_well[p].to_numpy().max())
-    dif_v = ymax - ymin
-
-    plt.ylim(ymin - (dif_v/10), ymax + dif_v)
-    plt.ylabel('Groundwater table (m)', fontsize = 12)
-    plt.xlabel('Years', fontsize = 12)
-    plt.legend(bbox_to_anchor=(1.0, 1.0), loc='upper left')
-    plt.title('Observation well - ' + str(p), fontsize = 16, fontweight='bold')
-
-    fs_text = 8
-    plt.text(13200, ymax + dif_v - dif_v/8, 'NSE:    ' + str(round(nse_w[0],3)), fontsize = fs_text)
-    plt.text(13200, ymax + dif_v - 2*(dif_v/8), 'KGE:    ' + str(round(kge_w[0],3)), fontsize = fs_text)
-    plt.text(13200, ymax + dif_v - 3*(dif_v/8), 'PBIAS: ' + str(round(pbias_w[0],3)), fontsize = fs_text)
-    plt.text(13200, ymax + dif_v - 4*(dif_v/8), 'RMSE: ' + str(round(rmse_w[0],3)) + ' $m^{3}/s$', fontsize = fs_text)
-    plt.text(13200, ymax + dif_v - 5*(dif_v/8), 'MAE:   ' + str(round(mae_w,3)) + ' $m^{3}/s$', fontsize = fs_text)
-    plt.text(13200, ymax + dif_v - 6*(dif_v/8), '$R^{2}$:      ' + str(round(r2_w,3)), fontsize = fs_text)
-
-    plt.savefig(os.path.join(path_results, best_experiment, 'Graficas', 'Obs_well_' + str(p) + '_' + methodology + '_' + best_experiment + '.png'))
-    plt.clf()
-"""
-
-
-
-
-"""
-#---    ERROR PORCENTUAL - K y Sy
-shape = gpd.read_file(os.path.join(path_results, best_experiment, best_result, 'iter_' + str(best_iteration), best_shape))
-shape_obs = gpd.read_file(r'..\data\GIS\Final_version\Elements_FA_vf.shp')
-
-for n in variables:
-    globals()[n + '_obs'] = shape_obs[n]
-    globals()[n + '_sim'] = shape[n]
-
-    locals()['dif_' + n] = abs(globals()[n + '_sim'] - globals()[n + '_obs'])   
-    globals()['error_' + n] = (locals()['dif_' + n] / globals()[n + '_obs']) * 100
-    #globals()['error_' + n] = globals()['error_' + n].fillna(0)
-
-    globals()['matriz_error_' + n] = (globals()['error_' + n].to_numpy()).reshape((84, 185))
-
-    #---    Graph
-    plt.figure(figsize = (16,8))
-    plt.imshow(globals()['matriz_error_' + n], cmap = 'viridis')
-
-    im_ratio = globals()['matriz_error_' + n].shape[0]/globals()['matriz_error_' + n].shape[1]
-    plt.colorbar(fraction=0.047*im_ratio, extend='max')
-    plt.clim(0, 100)
-
-    plt.title('Relative error (%) - ' + n)
-    plt.xlabel('Column')
-    plt.ylabel('Row')
-    
-    plt.savefig(os.path.join(path_results, best_experiment, 'Graficas', 'Error_relativo_' + n + '_' + methodology + '_' + best_experiment + '.png'))
-    plt.clf()
-    
-    #---    Graph 2
-    plt.figure(figsize = (16,8))
-    plt.imshow(globals()['matriz_error_' + n], cmap = 'viridis')
-
-    im_ratio = globals()['matriz_error_' + n].shape[0]/globals()['matriz_error_' + n].shape[1]
-    plt.colorbar(fraction=0.047*im_ratio, extend='max')
-    plt.clim(0, 1000)
-
-    plt.title('Relative error (%) - ' + n)
-    plt.xlabel('Column')
-    plt.ylabel('Row')
-    
-    plt.savefig(os.path.join(path_results, best_experiment, 'Graficas', 'Error_relativo_2_' + n + '_' + methodology + '_' + best_experiment + '.png'))
-    plt.clf()
-"""
